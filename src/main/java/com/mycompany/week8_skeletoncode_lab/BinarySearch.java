c89edadc20a8271ae9d91b20efd1d14d9aeda501
yaahboii <137734293+yaahboii@users.noreply.github.com>
2024-03-24 03:22:53 -0400

    binary committ

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
@@ -6,7 +6,7 @@ package com.mycompany.week8_skeletoncode_lab;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author Eric Paiz
  */
 public class BinarySearch {
 
@@ -27,7 +27,25 @@ public class BinarySearch {
         }
         return index;
     }
-    //ToDo 2: Call the above method and test the algorithm  
-    // provide time and space analysis 
 
+
+    public static void main(String[] args) {
+        int [] sortedArray = {3,8,13,29,42,91};
+        int key = 29;
+        int index = BinarySearch.runBinarySearchIteratively(sortedArray, key, 0, sortedArray.length-1);
+
+        if (index != Integer.MAX_VALUE) {
+            System.out.println("Index location of key: " + index);
+        }else {
+            System.out.println("Array does not contain the key");
+        }
+    }
 }
+
+
+
+// The time complexity is O(log n). With each loop, half the entries are eliminated, in a logarithmic manner.
+//
+
+
+
